2d6107d27947d66ae6f22dfa421adb5f1b86bd06
yangchuanlian <yangchuanlian@baidu.com>
2016-12-09 10:25:34 +0800

    create project

---
+++ src/main/java/com/baidu/ipangu/proxy/Main.java
@@ -0,0 +1,10 @@
+/*
+ * Copyright (C) 2016 Baidu, Inc. All Rights Reserved.
+ */
+package com.baidu.ipangu.proxy;
+
+/**
+ * Created by yangchuanlian on 2016/12/9.
+ */
+public class Main {
+}
---
+++ src/test/java/com/baidu/ipangu/http/MainTest.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright (C) 2016 Baidu, Inc. All Rights Reserved.
+ */
+package com.baidu.ipangu.http;
+
+import org.junit.Test;
+
+/**
+ * Created by yangchuanlian on 2016/12/9.
+ */
+public class MainTest {
+
+    @Test
+    public void testMethod() {
+        System.out.println("ok");
+    }
+}
